83c05975e69eda616ed9b5ad63da58c9e567433c
yesuxiong.ysx <yesuxiong.ysx@alibaba-inc.com>
2025-08-01 18:57:21 +0800

    feat: add liquid glass effect and image component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import LiquidGlass from "./components/liquid-glass/index.js";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import OldGlass from "./old-component/index.jsx";
+// import OldGlass from "./old-component/index.jsx";
 
 function App() {
   return (
     <div>
-      <OldGlass />
+      {/* <OldGlass /> */}
       <LiquidGlass>123</LiquidGlass>
+      <img
+        style={{ width: "100%", height: "100%" }}
+        src="https://fastly.picsum.photos/id/546/300/300.jpg?hmac=f8E2wXr3kthnt3BT6h17Y5Baf_0aJUdPIV7GqBVgxzY"
+        alt=""
+      />
     </div>
   );
 }
--- src/components/liquid-glass/index.tsx
+++ src/components/liquid-glass/index.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./index.css";
+import gsap from "gsap";
 
 interface LiquidGlassProps {
   children: React.ReactNode;
 }
 
 function LiquidGlass({ children }: LiquidGlassProps) {
-  return <div>{children}</div>;
+  const svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const serialized = new XMLSerializer().serializeToString(svgNode);
+  const encoded = encodeURIComponent(serialized);
+  const dataUri = `data:image/svg+xml,${encoded}`;
+  useEffect(() => {
+    gsap.set("feImage", {
+      attr: {
+        href: dataUri,
+      },
+    });
+  }, []);
+  return (
+    <div className="effect">
+      <div className="child-wrap">{children}</div>
+      <svg className="filter" xmlns="http://www.w3.org/2000/svg">
+        {/* 容器元素 */}
+        <defs>
+          {/* 滤镜元素 */}
+          <filter id="filter" colorInterpolationFilters="sRGB">
+            {/* 引入位移映射图像 */}
+            <feImage
+              x="0"
+              y="0"
+              width="100%"
+              height="100%"
+              result="map"
+            ></feImage>
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
 }
 
 export default LiquidGlass;
